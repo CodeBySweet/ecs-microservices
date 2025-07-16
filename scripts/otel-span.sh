8519e6dd2d25926adfacc46efd7452d1497cbfaa
shirinz-akumo <zsiyovuddinova@akumoproject.com>
2025-07-15 23:10:24 -0500

    Added self-hosted runner, Otel

--- scripts/otel-span.sh
+++ scripts/otel-span.sh
@@ -1,20 +1,36 @@
 #!/bin/bash
 
-NAME="$1"
-STATUS="$2"  # OK or ERROR
+SPAN_NAME="$1"
+STATUS="$2"
 
-curl -X POST http://localhost:4317/v1/traces \
-  -H 'Content-Type: application/json' \
-  -d '{
-    "resourceSpans": [{
-      "resource": { "attributes": [{ "key": "service.name", "value": { "stringValue": "github-actions" } }] },
-      "instrumentationLibrarySpans": [{
-        "spans": [{
-          "name": "'"$NAME"'",
-          "startTimeUnixNano": '$(($(date +%s%N)))',
-          "endTimeUnixNano": '$(($(date +%s%N)))',
-          "status": { "code": "'"$STATUS"'" }
-        }]
+# Current Unix timestamp in nanoseconds
+START_TIME=$(date +%s%N)
+END_TIME=$(date +%s%N)
+
+cat <<EOF > temp-span.json
+{
+  "resourceSpans": [{
+    "resource": {
+      "attributes": [
+        { "key": "service.name", "value": { "stringValue": "github-actions-runner" } },
+        { "key": "host.name", "value": { "stringValue": "$(hostname)" } }
+      ]
+    },
+    "scopeSpans": [{
+      "spans": [{
+        "name": "$SPAN_NAME",
+        "startTimeUnixNano": $START_TIME,
+        "endTimeUnixNano": $END_TIME,
+        "status": { "code": "$STATUS" }
       }]
     }]
-  }'
+  }]
+}
+EOF
+
+# Send span to OpenTelemetry Collector on localhost
+curl -s -X POST http://localhost:4317/v1/traces \
+  -H "Content-Type: application/json" \
+  --data-binary @temp-span.json
+
+rm temp-span.json
