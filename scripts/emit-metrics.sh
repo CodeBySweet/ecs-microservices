34a958a6976e069cf47b97565aedb0467f4b106d
shirinz-akumo <zsiyovuddinova@akumoproject.com>
2025-07-17 19:53:23 -0500

    Updated CI

---
+++ scripts/emit-metrics.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+METRIC_NAME="$1"
+METRIC_VALUE="$2"
+SERVICE="$3"
+JOB_NAME="$4"
+
+cat <<EOF | curl -s --data-binary @- http://localhost:4318/v1/metrics -H "Content-Type: application/json"
+{
+  "resourceMetrics": [{
+    "resource": {
+      "attributes": [
+        { "key": "service.name", "value": { "stringValue": "$SERVICE" } },
+        { "key": "job.name", "value": { "stringValue": "$JOB_NAME" } }
+      ]
+    },
+    "scopeMetrics": [{
+      "metrics": [{
+        "name": "$METRIC_NAME",
+        "type": "gauge",
+        "gauge": {
+          "dataPoints": [{
+            "asDouble": $METRIC_VALUE
+          }]
+        }
+      }]
+    }]
+  }]
+}
+EOF
